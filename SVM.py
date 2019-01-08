786a8ef2f040ac51c7beb2afe6dba9c2a242661e
parkjh4550 <37648792+parkjh4550@users.noreply.github.com>
2019-01-08 20:07:58 +0900

    Initial commit

---
+++ SVM.py
@@ -0,0 +1,212 @@
+#ID : 2018221270
+#NAME : jonghoon park
+
+from sklearn.svm import SVC
+import numpy as np
+
+from sklearn import datasets
+import numpy as np
+import datasplit
+import copy
+
+def get_data():
+    fold_num = 3  # 50 data for each folds( 150 / 3 )
+
+    iris = datasets.load_iris()
+    X = list(iris.data[:, :])  # 150sampels, 4 features
+    y = list(iris.target)  # 3 kinds of target
+
+    # 1-1 : split into 3 folds
+    # ( data ) -> (fold1, fold2, fold3 )
+    data_folds, target_folds = datasplit.divide_fold(X, y, fold_num)
+
+    # 1-2 : split into train and test for each fold
+    # ( fold ) -> ( train, test )
+    num_test = int(len(data_folds[0]) / 5)  # 10 test data for each fold
+    num_train = int(len(data_folds[0]) - num_test)  # 40 train data for each fold
+    data_folds, target_folds = datasplit.get_train_and_test(data_folds, target_folds, fold_num, num_train=num_train,
+                                                  num_test=num_test)
+
+    # ( train, test ) -> ( (train1, train2 ... train5) , test )
+    num_val = int(len(data_folds[0][0]) / 5)  # 8 val data for each fold
+    num_train = int(len(data_folds[0][0]) - num_val)  # 32 val data for each fold
+    data_folds, target_folds = datasplit.get_trainfold(data_folds, target_folds, fold_num, num_train=num_train, num_val=num_val)
+
+    return data_folds, target_folds
+
+def get_valdata(data, target, val_fold):
+    # divide train data into train, validation
+
+    train_data = copy.deepcopy(data[0])
+    train_target = copy.deepcopy(target[0])
+
+    val_data = train_data.pop(val_fold)
+    val_target = train_target.pop(val_fold)
+
+    tmp_d, tmp_t = [], []
+    for itr in range(len(train_data)):
+        tmp_d.extend(train_data[itr])
+        tmp_t.extend(train_target[itr])
+
+    train_data = tmp_d
+    train_target = tmp_t
+
+    return train_data, train_target, val_data, val_target
+
+def get_param():
+    # return parameters for grid search
+    # kernel, C, degree, coef0, gamma
+    param_list = []
+    for kernel in ['poly', 'rbf']:
+        for c in [1,10,100]:
+            for degree in [3,4,5,20]:
+                for coef0 in [0, 10, 100]:
+                    for gamma in [0.01, 0.001, 0.0001]:
+                        param_list.append([kernel, c, degree, coef0, gamma])
+
+    return param_list
+
+
+
+
+def find_proper_parameter(data, target, num_mixture):
+    # data : data of folds0 ( [ train, test] )
+        # train : [ train1, train2, ...., train5 ]
+    # target : target of folds0 ( [ train, test] )
+        # train : [ train1, train2, ...., train5 ]
+
+    tot_data = copy.deepcopy(data)
+    tot_target = copy.deepcopy(target)
+
+    total_acc = []
+
+    param = get_param()
+
+    for itr in range(len(param)):
+
+        accuracy = []
+        for i in range(len(data[0])):
+            #create Gaussian Mixture model for each condition
+            # C = 1, 10 , 100 ...
+            # kernel = 'poly', 'rbf'
+            # degree = 3, 4, 5 ... (in case of 'poly')
+            # gamma = 'auto', 'scale'
+            # coef0 = 0, 10, 100
+
+            #print(param[itr])
+            model = SVC(kernel=param[itr][0], C=param[itr][1],  degree=param[itr][2], coef0 = param[itr][3], gamma=param[itr][4], random_state=1)
+            train_data, train_target, val_data, val_target = get_valdata(tot_data, tot_target, val_fold=int(i))
+            #print(itr, train_target)
+            #print('train : ', train_data)
+            model.fit(X=train_data, y=train_target)
+            # find clustered number
+            train_predict = model.predict(train_data)
+
+            c_list = []
+
+            # get the predidcted class by train_data
+            for c in range(3):
+                c_index = list(np.where(np.array(train_target) == c)[0]) #find the data index of each class
+                if c_index:
+                    p_class = train_predict[c_index[0]]
+                    if p_class in c_list:
+                        c_list.extend([20])     # if there is no match target, then give trash value
+                    else:
+                        c_list.extend([p_class])
+
+                if not c_index: #if there is no matched index
+                    c_index.extend([20])        # if there is no match target, then give trash value
+
+
+            #chanege the target class to match trained class
+            target = []
+            for t in val_target:
+                if t == 0: target.append(c_list[0])
+                elif t == 1: target.append(c_list[1])
+                elif t == 2: target.append(c_list[2])
+
+            predict = model.predict(val_data)
+            acc = float( len(list(np.where(np.array(target) == predict)[0])) )
+            acc = acc/ 8.
+
+            accuracy.append(acc)
+
+        model_acc = sum(accuracy)/len(accuracy)
+
+        total_acc.append(model_acc)
+
+    print('accuracy of each model done by validation set: ', total_acc)
+    max_acc = max(total_acc)
+    max_num = total_acc.index(max_acc)
+
+    for itr, p in enumerate(param):
+        print('param : ', p)
+        print('accuracy : ', total_acc[itr])
+
+    return param[max_num]
+
+def get_test(data, target, param):
+    train_data = copy.deepcopy(data[0])
+    train_target = copy.deepcopy(target[0])
+
+    test_data = copy.deepcopy(data[1])
+    test_target = copy.deepcopy(target[1])
+
+    tmp_d, tmp_t = [], []
+    for i in range(len(train_data)):
+        tmp_d = tmp_d + train_data[i]
+        tmp_t = tmp_t + train_target[i]
+    train_data = tmp_d
+    train_target = tmp_t
+
+    model = model = SVC(kernel=param[0], C=param[1],  degree=param[2], coef0 = param[3], random_state=1)
+    model.fit(train_data, train_target)
+
+    # find clustered number
+    train_predict = model.predict(train_data)
+    c_list = []
+    # get the predidcted class by train_data
+    for c in range(3):
+        c_index = list(np.where(np.array(train_target) == c)[0])  # find the data index of each class
+        if c_index:
+            p_class = train_predict[c_index[0]]
+            if p_class in c_list:
+                c_list.extend([20])  # if there is no match target, then give trash value
+            else:
+                c_list.extend([p_class])
+
+        if not c_index:  # if there is no matched index
+            c_index.extend([20])  # if there is no match target, then give trash value
+
+    # chanege the test target class -> match trained class
+    target = []
+    for t in test_target:
+        if t == 0:
+            target.append(c_list[0])
+        elif t == 1:
+            target.append(c_list[1])
+        elif t == 2:
+            target.append(c_list[2])
+
+    predict = model.predict(test_data)
+    acc = float( len(list(np.where(np.array(target) == predict)[0])) )
+    acc = acc/ (len(target))
+
+    return acc
+
+if __name__ == '__main__':
+    # homework1 :  1~10, possible num of mixtures
+    num_mixture = 10
+
+    data_folds, target_folds = get_data()
+
+    # homework2 : make Gaussian mixture and  train
+    # 2-1: make Gaussain Mixture
+    data = data_folds[0]
+    target = target_folds[0]
+
+    best_param = find_proper_parameter(data, target, num_mixture)
+    print('selected param : ', best_param)
+
+    test_acc = get_test(data, target, best_param)
+    print('final total accuracy : ', test_acc)
---
+++ datasplit.py
@@ -0,0 +1,108 @@
+#ID : 2018221270
+#NAME : jonghoon park
+
+
+from sklearn import datasets
+from sklearn.utils import shuffle
+
+import numpy as np
+
+def divide_fold(data, target, fold_num):
+    # get total_data and divide into 5 folds
+    # data : list , total data list
+    # target : list, total target of data
+    # fold_num : number of folds
+
+    # shuffle the dataset not disturbing the mapping
+    data, target = shuffle(data, target, random_state=0)
+
+    data_folds, target_folds = [], []
+    data_num = int(len(data)/fold_num)
+    for i in range(fold_num):
+        start = data_num * i
+        tmp_d = data[start : start + data_num]
+        tmp_t = target[start : start + data_num]
+        data_folds.append(tmp_d)
+        target_folds.append(tmp_t)
+    return data_folds, target_folds
+
+def get_train_and_test(data, target, fold_num, num_train, num_test):
+    # data : list
+        #folds -> ( data )
+    # target : list
+        #folds -> ( data )
+    split_data = []     # ( train, test )
+    split_target = []   # ( train, test )
+
+    num_data = len(data[0])
+    for i in range(fold_num):
+
+        # [ train_data, test_data ] for each fold
+        tmp_d = [ data[i][0:num_train] , data[i][num_train : num_train + num_test]]
+        tmp_t = [ target[i][0:num_train], target[i][num_train : num_train + num_test]]
+
+        split_data.append(tmp_d)
+        split_target.append(tmp_t)
+
+    return split_data, split_target
+
+def get_trainfold(data, target, fold_num, num_train, num_val):
+    # data : list ( train, test )
+        #train, test : list
+    # target : list ( train, test )
+        #train, test : list
+
+    split_data = []     # ( train, val, test )
+    split_target = []   # ( train, val, test )
+
+    for i in range(fold_num):
+        tmp_train_d = []
+        tmp_target_d = []
+        for j in range(5):
+            start = j * num_val
+            tmp_train_d.append(data[i][0][start : start+num_val])
+            tmp_target_d.append(target[i][0][start : start+num_val])
+
+        tmp_d = [tmp_train_d, data[i][1]]
+        tmp_t = [tmp_target_d, target[i][1]]
+
+        split_data.append(tmp_d)
+        split_target.append(tmp_t)
+
+    return split_data, split_target
+if __name__ == '__main__':
+
+    fold_num = 3    #50 data for each folds( 150 / 3 )
+
+    iris = datasets.load_iris()
+    X = list(iris.data[:,:])      # 150sampels, 4 features
+    y = list(iris.target)         # 3 kinds of target
+
+    print('-----------homwork1')
+    # homework 1
+    # 1-1 : split into 3 folds
+    # ( data ) -> (fold1, fold2, fold3 )
+    data_folds, target_folds = divide_fold(X, y, fold_num)
+    print('splitted data folds number : ', len(data_folds))
+
+    # 1-2 : split into train and test for each fold
+    # ( fold ) -> ( train, test )
+    num_test = int(len(data_folds[0]) / 5)  # 10 test data for each fold
+    num_train = int(len(data_folds[0]) - num_test)  # 40 train data for each fold
+    data_folds, target_folds = get_train_and_test(data_folds, target_folds, fold_num, num_train = num_train, num_test= num_test)
+    print('number of train data for each fold : ', len(data_folds[0][0]))
+    print('number of test data for each fold : ', len(data_folds[0][1]))
+
+    print('\n--------------homeworkd2')
+    # homework 2
+    # 2-1 : split train into train and validation for each fold
+    # ( train, test ) -> ( (train1, train2 ... train5) , test )
+    num_val = int(len(data_folds[0][0]) / 5)    # 8 val data for each fold
+    num_train = int(len(data_folds[0][0]) - num_val)    # 32 val data for each fold
+    data_folds, target_folds = get_trainfold(data_folds, target_folds, fold_num, num_train=num_train, num_val=num_val)
+
+    print('train)number of fold for each fold : ', len(data_folds[0][0]))
+    print('train)number of data for each train fold : ', len(data_folds[0][0][0]))
+    print('train)number of total data : ', len(data_folds[0][0]) * len(data_folds[0][0][0]))
+
+    print('test)number of data for each fold : ', len(data_folds[0][1]))
